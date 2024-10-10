3c0af291bc1938cfa31078a24521e3e51ce86f5f
aracelihernandez01 <ahernandez60@hawk.iit.edu>
2024-10-10 16:37:45 -0500

    typo correction

--- css/script.js
+++ css/script.js
@@ -5,10 +5,13 @@ window.onload = function() {
         form.addEventListener('submit', function(event) {
             event.preventDefault();
             const formData = new FormData(form);
-            let message = 'You have successfully registered for monthly Youth Events. Click "OK" if the following information is correct. Thank you!\n';
+
+            let message ='You have successfully registered for monthly Youth Events. Click "OK" if the following information is correct. Thank you!\n';
             for (let pair of formData.entries()) {
                 message += `${pair[0]}: ${pair[1]}\n`;
             }
             console.log(message)
             alert(message);
         });
+    }
+};
