852c2e7e1232b5e63749621d7c34613e70515a42
aracelihernandez01 <ahernandez60@hawk.iit.edu>
2024-10-10 16:18:35 -0500

    message construction added

--- css/script.js
+++ css/script.js
@@ -5,3 +5,10 @@ window.onload = function() {
         form.addEventListener('submit', function(event) {
             event.preventDefault();
             const formData = new FormData(form);
+            let message = 'You have successfully registered for monthly Youth Events. Click "OK" if the following information is correct. Thank you!\n';
+            for (let pair of formData.entries()) {
+                message += `${pair[0]}: ${pair[1]}\n`;
+            }
+            console.log(message)
+            alert(message);
+        });
