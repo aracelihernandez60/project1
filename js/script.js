70e3bd2d808cbfd92aae6e93e848ab175cf2d6af
aracelihernandez01 <ahernandez60@hawk.iit.edu>
2024-10-31 14:45:39 -0500

    empty space removed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 window.onload = function() {
     const form = document.getElementById('signup-form');
-
     if (form) {
         form.addEventListener('submit', function(event) {
             event.preventDefault();
             const formData = new FormData(form);
-
             let message ='You have successfully registered for monthly Youth Events. Click "OK" if the following information is correct. Thank you!\n';
             for (let pair of formData.entries()) {
                 message += `${pair[0]}: ${pair[1]}\n`;
